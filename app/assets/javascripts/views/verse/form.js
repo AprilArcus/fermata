f8c2a445881d8fa3cd0f798d8fd312e9b99d8cfc
April Arcus <april.arcus@gmail.com>
2014-07-30 11:54:09 -0700

    fixed drag proxies and cursors

--- app/assets/javascripts/views/verse/form.js
+++ app/assets/javascripts/views/verse/form.js
@@ -36,13 +36,59 @@ Dianthus.Views.VerseForm = Backbone.CompositeView.extend({
       return testElement.style.cursor;
     });
 
-    var $measureLoopsList = this.$el.find('.measure-loops-list');
-    $measureLoopsList.sortable({ connectWith: '.measure-loops-list',
-                                 handle: '.measure-loop-title',
-                                 placeholder: 'measure-loop-placeholder ui-corner-all',
-                                 revert: 150,
-                                 scroll: false,
-                                 cursor: grabbingCursor });
+    var $measureLoopsLists = this.$el.find('.measure-loops-list');
+    $measureLoopsLists.sortable({ connectWith: '.measure-loops-list',
+                               // prevent dragging by dropdown menu background
+                                  handle: '.measure-loop-title',
+                                  placeholder: 'measure-loop-placeholder ui-corner-all',
+                                  revert: 150,
+                                  scroll: false,
+                                  cursor: grabbingCursor,
+ // sortable's default behavior prepends an anonymous
+ // <style> *{cursor: foo-cursor !important} </style> element into the
+ // bottom of <body>, making it impossible to override in CSS. The
+ // intent is apparently for sortable's cursor to override any custom
+ // cursor inherited by the drag helper, but this results in sortable's
+ // cursor taking precedence over the cursor of a connected draggable,
+ // which we do not want here.
+                                  start: function(event, ui) {
+ // did we originate from a drag event?
+    if (/draggable/.test(event.originalEvent.target.className)) {
+      console.log(event.originalEvent);
+      $('body > style:last-child').empty(); // clear the offending <style>
+      document.body.style.cursor = 'alias'; // reset the original cursor
+    }
+     
+                                  } });
+ // The consequence of this is that we must set up our CSS very
+ // carefully, knowing that JQueryUI will set {cursor: foo-cursor}
+ // on <body> and bootstrap will set {cursor: pointer} on .btn (with
+ // which .loop-title and .measure-loop-title coexist as of 2014-07-30):
+
+ //   .loop-title {
+ //     cursor: default;
+ //   }
+
+ //   .measure-loop-title {
+ //     cursor: move;
+ //     cursor: -moz-grab;
+ //     cursor: -webkit-grab;
+ //     cursor: grab;
+ //   }
+
+ //   .ui-draggable-dragging .loop-title,
+ //   .ui-sortable-helper .measure-loop-title {
+ //     cursor: inherit !important;
+ //   }
+
+    var $loops = this.$el.find('.loop');
+    $loops.draggable({ 
+                       cursor: 'alias',
+                       connectToSortable: '.measure-loops-list',
+                       // handle: '.loop-title',
+                       helper: 'clone',
+                     });
+                      
 
     return this;
   }
